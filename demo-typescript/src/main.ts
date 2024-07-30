a0850f6eeec193ad92b95e848ecbba357f7c662f
ihab abadi <ihab@utopios.net>
2024-07-30 17:49:38 +0200

    with node package configuration

---
+++ demo-typescript/src/main.ts
@@ -0,0 +1,24 @@
+const firstname:string = "ihab"
+
+let anyValue : any = 10
+
+anyValue = "test"
+
+const names: string[] = ["name1", "name2"]
+
+names.push(firstname)
+
+
+function printId(id: number | string) {
+    if (typeof id === "string") {
+  // id is of type string
+  console.log(id.toUpperCase());
+    } else {
+  // id is of type number
+  console.log(id);
+    }
+  }
+
+  printId(10)
+  printId("test")
+  printId("new value")
