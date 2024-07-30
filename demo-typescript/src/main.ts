8df7fd00b75926c5da3acf0be09483444f3898ec
ihab abadi <ihab@utopios.net>
2024-07-30 18:45:30 +0200

    solution lab 3

--- demo-typescript/src/main.ts
+++ demo-typescript/src/main.ts
@@ -1,51 +1,93 @@
-const firstname:string = "ihab"
+// const firstname:string = "ihab"
 
-let anyValue : any = 10
+// let anyValue : any = 10
 
-anyValue = "test"
+// anyValue = "test"
 
-const names: string[] = ["name1", "name2"]
+// const names: string[] = ["name1", "name2"]
 
-names.push(firstname)
+// names.push(firstname)
 
 
-function printId(id: number | string) {
-    if (typeof id === "string") {
-  // id is of type string
-  console.log(id.toUpperCase());
-    } else {
-  // id is of type number
-  console.log(id);
+// function printId(id: number | string) {
+//     if (typeof id === "string") {
+//   // id is of type string
+//   console.log(id.toUpperCase());
+//     } else {
+//   // id is of type number
+//   console.log(id);
+//     }
+//   }
+
+//   printId(10)
+//   printId("test")
+//   printId("new value")
+
+
+// type PointType = {
+//     x: number,
+//     y: number
+// }
+
+// interface PointInterface {
+//     x:number,
+//     y:number
+// }
+
+// interface PointInterface {
+//     z:number
+// }
+
+
+
+// type Point = PointInterface 
+
+// const element = {x:10, y:100}
+
+// const newElement = element as Point
+
+
+// Solution of lab3
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+type Person = User | Admin
+
+const persons : Person[] = [
+    {
+        name: 'Toto Dupont',
+        age: 35,
+        occupation: 'Postman'
+    },
+    {
+        name: 'Jeanne Doe',
+        age: 25,
+        role: 'Admin'
     }
-  }
+]
 
-  printId(10)
-  printId("test")
-  printId("new value")
-
-
-type PointType = {
-    x: number,
-    y: number
+function isUser(obj: any): obj is User {
+    return obj && typeof obj.name === 'string' && typeof obj.age === 'number' && typeof obj.occupation ===  'string'
 }
 
-interface PointInterface {
-    x:number,
-    y:number
-}
-
-interface PointInterface {
-    z:number
-}
-
-
-
-type Point = PointInterface 
-
-const element = {x:10, y:100}
-
-const newElement = element as Point
-
-
-
+persons.forEach(person => {
+    console.log(`Name: ${person.name}, Age: ${person.age}`)
+    if(isUser(person)) {
+        const val = person as User
+        console.log(`Occupation: ${val.occupation}`)
+    }else {
+        const val = person as Admin
+        console.log(`Role: ${val.role}`)
+    }
+   
+})
 
